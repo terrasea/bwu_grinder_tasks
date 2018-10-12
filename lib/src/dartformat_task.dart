9919a341710ebdc58fb3fdaea83daf86b7c32d2b
James Hurford <terrasea@gmail.com>
2018-10-12 10:47:52 +0100

    Fix for dartformat_task

--- lib/src/dartformat_task.dart
+++ lib/src/dartformat_task.dart
@@ -5,7 +5,7 @@ import 'package:grinder/grinder.dart';
 /// Check whether all *.dart files in the given directories and their sub-
 /// directories are properly formatted.
 void checkFormatTask(Iterable<String> directories) {
-  final output = PubApp.global('dart_style')
+  final output = new PubApp.global('dart_style')
       .run(['--dry-run']..addAll(directories), script: 'format');
   if (output.split('\n').where((l) => l.isNotEmpty).isNotEmpty) {
     context.fail('Some files are not properly formatted.');
